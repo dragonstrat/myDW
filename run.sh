2f8d9a2031ca3c987d33827a76ff18bc867e625a
Ganghee Jang <gangheej@uci.edu>
2016-11-10 20:09:15 -0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@
 # I'll execute my programs, with the input directory paymo_input and output the files in the directory paymo_output
 # python ./src/antifraud.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
 ./src/antifraud ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
+
